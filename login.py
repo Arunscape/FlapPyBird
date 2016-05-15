fc2375ec99abb60977d7af9146000e182999e2f0
Arunscape <arunscape@gmail.com>
2016-05-15 11:53:44 -0600

    added code to new account button in login.py added hiscores.py, code will be added later

--- login.py
+++ login.py
@@ -31,40 +31,93 @@ class loginApp:
         self.newacbutton = Button(botframe, text='New Account', command=self.new_acc)
         self.newacbutton.grid(row=0, column=2, sticky=E)
 
-        self.ntries = 4
+        ##end of gui making
+
+        #opens csv file
+        with open('scores.csv') as csvfile:
+            self.data = list(csv.reader(csvfile))
+
+        self.ntries = 4 #counter for incorrect attempts
+        self.clicked_new_account_before = False
 
     def log_in(self):
-       with open('scores.csv') as csvfile:
-            data = list(csv.reader(csvfile))
+        if not self.are_fields_empty():
+            if self.validate_credentials():
+                print('success! logging in...')
+            elif not self.validate_credentials():
+                self.username_exists()
 
-            usernm = self.userentry.get()
-            passwd = self.passentry.get()
 
-
-            if usernm == '' or passwd == '':
-                messagebox.showerror('Empty Fields','You left either your username or password empty! Without both, you cannot log in!!!')
-            elif usernm != '' and passwd != '':
-                if not any(usernm in line for line in data):
-                    if messagebox.askyesno('Username does not exist', 'Would you like to make a new account?'):
-                        self.new_acc()
-                else:
-                    for index, line in enumerate(data):
-                        if usernm == line[0]:
-                            usernmindex = index
-                            print('found username at index %s in data' %index)
-                            print('password should be %s' %(data[usernmindex][1]))
-                    if passwd == data[usernmindex][1]:
-                        print('success! logging you in...')
-                    elif self.ntries > 1:
-                        self.ntries -= 1
-                        messagebox.showerror('WRONG PASSWORD ENTERED', 'YOU HAVE %s TRIES REMAINING' %self.ntries)
-
-                    else:
-                        messagebox.showerror('INTRUDER ALERT', 'UNAUTHORIZED USER DETECTED, PROGRAM EXITING...')
-                        root.destroy()
     def new_acc(self):
-        print('making a new account, now all I need to do is code')
+        if not self.clicked_new_account_before:
+            if not self.are_fields_empty():
+                if self.username_available():
+                    if self.doublecheck_passowrd():
+                        return
 
+        elif self.clicked_new_account_before == True:
+            if self.verify_new_password():
+                print('meking new account...')
+            elif not self.verify_new_password():
+                self.try_again()
+
+    def username_exists(self):
+        if any(self.userentry.get() == line[0] for line in self.data):
+            self.intruder_alert()
+            return True
+        else:
+            if messagebox.askyesno('Username does not exist!', 'The username you entered does not exist! Would you like to make a new account?'):
+                self.new_acc()
+            return False
+
+
+    def are_fields_empty(self):
+        if self.userentry.get() == '' or self.passentry.get() == '':
+            messagebox.showerror('Empty Fields','You left either your username or password empty! Without both, you cannot log in!!!')
+            return True
+        else: return False
+
+    def validate_credentials(self):
+        for line in self.data:
+            if self.userentry.get() == line[0] and self.passentry.get() == line[1]:
+                return True
+            #else: return False
+
+    def intruder_alert(self):
+        if self.ntries > 1:
+            self.ntries -= 1
+            messagebox.showerror('WRONG PASSWORD ENTERED', 'YOU HAVE %s TRIES REMAINING' % self.ntries)
+        else:
+            messagebox.showerror('INTRUDER ALERT', 'UNAUTHORIZED USER DETECTED, PROGRAM EXITING...')
+            root.destroy()
+
+    def username_available(self):
+        if any(self.userentry.get() == line[0] for line in self.data):
+            messagebox.showerror('Username taken!', 'The username you entered is taken by someone else, try a different one!')
+            self.passentry.delete(0,'end')
+            self.userentry.delete(0,'end')
+            return False
+        else: return True
+
+    def doublecheck_passowrd(self):
+        messagebox.showinfo('Retype password', 'Please confirm your password.')
+        self.oldpass = self.passentry.get()
+        self.passentry.delete(0, 'end')
+        self.clicked_new_account_before = True
+        self.loginbutton.configure(state='disabled')
+        return True
+
+    def verify_new_password(self):
+        if self.oldpass == self.passentry.get():
+            return True
+        else: return False
+
+    def try_again(self):
+        messagebox.showerror('Passwords do not match', 'The passwords do not match, try again')
+        oldpass = ""
+        self.clicked_new_account_before = False
+        self.passentry.delete(0,'end')
+        self.loginbutton.configure(state='normal')
 
 
 

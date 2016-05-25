ff8832591931b58fd3177dc065a28b4e8614d44d
Arunscape <arunscape@gmail.com>
2016-05-25 19:42:27 +0000

    added radiobuttons to hiscores.py

--- hiscores.py
+++ hiscores.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from tkinter import *
 
+
 class hiscore:
   def __init__(self, master):
     #makes the gui
@@ -13,17 +14,26 @@ class hiscore:
     botframe.pack()
 
 
-    self.bg_image = PhotoImage(file ="assets/sprites/background-night.png")
-    self.x = Label (topframe, image = self.bg_image)
-    #self.x.place(x=0, y=0, relwidth=1 ,relheight=1)
-    self.x.pack()
-
-
     self.tryagainbutton = Button(botframe, text='Try again', command=self.try_again)
     self.tryagainbutton.grid()
     self.exitbutton = Button(botframe, text = 'Exit' , command=self.exit)
     self.exitbutton.grid(row = 0, column = 1)
 
+    v = IntVar()
+    self.label = Label(topframe, text='Sort by:')
+    self.label.grid(row = 0, column = 0)
+    
+    def sort_name():
+      print('sortname')
+    def sort_score():
+      print('sortscore')
+
+    Radiobutton(topframe, text='Name', variable = v, value = 1, command=sort_name).grid(row=0, column=1)
+    Radiobutton(topframe, text='Score', variable = v, value =2, command=sort_score).grid(row=0,column=2)
+
+    
+    
+    
 
 
   def try_again(self):

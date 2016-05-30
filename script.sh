eca72fce905f8d411871296d302c66128c5e32d3
Arunscape <Arunscape@users.noreply.github.com>
2016-05-30 18:48:28 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@ cd
 sudo add-apt-repository universe
 sudo add-apt-repository multiverse
 sudo add-apt-repository ppa:mystic-mirage/pycharm
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install python python3 python-tk python3-tk git pycharm-community mercurial-y
 hg clone https://bitbucket.org/pygame/pygame
 cd pygame
-sudo apt-get install python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev \
+sudo apt-get install -y python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev \
   libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev \
   libavformat-dev libswscale-dev libjpeg-dev libfreetype6-dev
 python3 setup.py build
